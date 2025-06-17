f50e7055a14f8dfd64f2c733e226dc33c1d7eb85
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-06-17 16:36:26 +0700

    feat(postgres): add vps sql scripts

--- postgres/insert_datas.sh
+++ postgres/insert_datas.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-psql -U postgres -d web_honeypot -a -f ./insert.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_ips.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_methods.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_paths.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_ports.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_sess_uuids.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_user_agents.sql
-psql -U postgres -d web_honeypot -a -f ./assoc_rules_uuids.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_attack_types.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_attack_types_large.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_methods.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_paths.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_paths_large.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_ports.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_ports_large.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_sess_uuids.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_sess_uuids_large.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_user_agents.sql
+psql -U postgres -d web_honeypot -a -f ./zap/zap_assoc_rules_user_agents_large.sql
+psql -U postgres -d web_honeypot -a -f ./zap/insert.sql
+
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_ports.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_ports_large.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_sess_uuids.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_sess_uuids_large.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_user_agents.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_attack_types.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_ips.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_logs.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_methods.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_paths.sql
+psql -U postgres -d web_honeypot -a -f ./vps/assoc_rules_paths_large.sql
+psql -U postgres -d web_honeypot -a -f ./vps/web_traffic_logs.sql
---
+++ postgres/vps/assoc_rules_methods.sql
@@ -0,0 +1,67 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5
+-- Dumped by pg_dump version 17.5
+
+-- Started on 2025-06-17 15:52:23
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 220 (class 1259 OID 16865)
+-- Name: assoc_rules_methods; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.assoc_rules_methods (
+    id integer NOT NULL,
+    support real NOT NULL,
+    method character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.assoc_rules_methods OWNER TO postgres;
+
+--
+-- TOC entry 4917 (class 0 OID 16865)
+-- Dependencies: 220
+-- Data for Name: assoc_rules_methods; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.assoc_rules_methods (id, support, method) FROM stdin;
+0	0.9705882	['GET']
+1	0.04901961	['POST']
+2	0.019607844	['GET', 'POST']
+\.
+
+
+--
+-- TOC entry 4771 (class 2606 OID 16869)
+-- Name: assoc_rules_methods assoc_rules_methods_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.assoc_rules_methods
+    ADD CONSTRAINT assoc_rules_methods_pkey PRIMARY KEY (id);
+
+
+-- Completed on 2025-06-17 15:52:23
+
+--
+-- PostgreSQL database dump complete
+--
+
