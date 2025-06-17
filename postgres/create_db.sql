82dc84d55ab6505f88e62d6936d388906c6d9114
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-06-17 19:39:03 +0700

    fix(postgres): insert_datas.sh databases

--- postgres/create_db.sql
+++ postgres/create_db.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE web_honeypot_vps;
 CREATE DATABASE web_honeypot_zap;
+CREATE DATABASE web_honeypot_nikto;
+CREATE DATABASE web_honeypot_nuclei;
--- postgres/insert_datas.sh
+++ postgres/insert_datas.sh
@@ -2,3 +2,6 @@
 
 psql -U postgres -a -f ./create_db.sql
 psql -U postgres -d web_honeypot_vps -a -f ./insert_vps.sql
+psql -U postgres -d web_honeypot_zap -a -f ./insert_zap.sql
+psql -U postgres -d web_honeypot_nikto -a -f ./insert_nikto.sql
+psql -U postgres -d web_honeypot_nuclei -a -f ./insert_nuclei.sql
